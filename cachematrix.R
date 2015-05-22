4b8dca04ed306b5a32f46ada935f27d546cce60c
NaomiS <naomiseng@hotmail.com>
2015-05-21 22:19:55 -0400

    Completed PA2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mx = matrix()) {
+  #create s object with a NULL value, to hold 'solved' matrix
+  s <- NULL
+ 
+  #The 'set' function may not be needed here, as it is not called by cachesolve.
+  #It has been included in case it is needed for the evalation of this assignment.
+  set <- function(y) {
+    mx <<- y
+    s <<- NULL
+  }
+  
+  #get will return the value of the matrix
+  get <- function() {
+    mx
+  }
+  
+  #setinverse caches the solved matrix
+  setinverse <- function(solve) {
+    s <<- solve
+  }
+  
+  #get inverse gets the solved matrix
+  getinverse <- function() {
+    s
+  }
+  
+  #The output is a list of functions
+  list(set = set, get = get, setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...) {
+  #THe input (x) should be the variable to which the output from
+  #makeCacheMatrix is stored
+  
+  #test to see if s is NULL. If it is not, dipslay a message
+  s <- x$getinverse()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  
+  #if s is NULL, the matrix is retrieved and solved. The output is cached to 
+  #the s variable and the value of s is printed to the screen.
+  else {
+  data <- x$get()
+  s <- solve (data,...)
+  x$setinverse(s)
+  s
+  }
 }
